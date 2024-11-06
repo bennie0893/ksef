db78b8a75466645ed8e2d04cfd09cfa555d198a9
bennie0893 <138223716+bennie0893@users.noreply.github.com>
2024-11-06 13:54:05 +0530

    Update and rename SERVER.JS to server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 // Middleware
 app.use(bodyParser.json());
