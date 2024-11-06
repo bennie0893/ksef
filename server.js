7eea5c6ae019d6c25e788abd2468ef64fadc36a6
bennie0893 <138223716+bennie0893@users.noreply.github.com>
2024-11-06 14:49:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.json());
